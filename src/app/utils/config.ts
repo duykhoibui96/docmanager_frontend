0dfbe0773097c8e81b62470ad9e95008bc4537d6
buiduykhoi <duykhoi.bui96@gmail.com>
2017-11-07 12:43:25 +0700

    Config url

--- src/app/utils/config.ts
+++ src/app/utils/config.ts
@@ -2,8 +2,8 @@ import { Routes, Resolve } from "@angular/router";
 
 export class CONFIG {
 
-    static SERVER_HOST = "http://localhost:3000";
+    static SERVER_HOST = `http://${window.location.hostname}:3000`;
     static API_URL = "/api";
-    static FULL_API_URL = "http://localhost:3000/api";
+    static FULL_API_URL = `http://${window.location.hostname}:3000/api`;
 
 }
